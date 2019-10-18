54232b818040883cf041f08de01a98ab375640d8
TwistedIO <49755561+TwistedIO@users.noreply.github.com>
2019-10-17 21:53:54 -0500

    Update GPT2-Learning.py

--- GPT2-Learning.py
+++ GPT2-Learning.py
@@ -127,10 +127,10 @@ def learnreset(bot, update):
 def regex(mew):
     meow = mew
     if "Me:" in meow:
-        meow = meow.rsplit('Me:', 1)[0]
+        meow = meow.rsplit('Me:')[0]
         return meow
     if "You:" in meow:
-        meow = meow.rsplit('You:', 1)[0]
+        meow = meow.rsplit('You:')[0]
         return meow
     if "?" in meow:
         meow = meow.rsplit('?', 1)[0]
